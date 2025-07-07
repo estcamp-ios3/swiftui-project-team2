a1d549c013e69b3a83071938b8b93da49b3ed81a
권태우 TaewooKéplerKwon <kwontaewoo0901@gmail.com>
2025-07-07 09:14:34 +0900

    Create new project

---
+++ ShadowQuiz/ShadowQuiz/ShadowQuizApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShadowQuizApp.swift
+//  ShadowQuiz
+//
+//  Created by 권태우 on 7/7/25.
+//
+
+import SwiftUI
+
+@main
+struct ShadowQuizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
