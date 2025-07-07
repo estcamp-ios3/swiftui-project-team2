b875bf0e26abe6fcc7e4b9a7c8c0059f12435896
권태우 TaewooKéplerKwon <kwontaewoo0901@gmail.com>
2025-07-07 11:01:29 +0900

    Update RankingView.swift

--- ShadowQuiz/ShadowQuiz/RankingView.swift
+++ ShadowQuiz/ShadowQuiz/RankingView.swift
@@ -11,9 +11,43 @@ struct RankingView: View {
     var body: some View {
         Text("RANKING")
             .font(.largeTitle)
+            .padding()
         Grid{
+            GridRow{
+                Text("Rank")
+                Text("Nickname")
+                Text("Score")
+            }
+            .padding()
             
+            GridRow{
+                Text("1")
+                Text("MIDKING")
+                Text("300")
+            }
+            GridRow{
+                Text("2")
+                Text("JUGKING")
+                Text("270")
+            }
+            GridRow{
+                Text("3")
+                Text("hide on bush")
+                Text("200")
+            }
+            GridRow{
+                Text("4")
+                Text("DK Képler")
+                Text("180")
+            }
+            GridRow{
+                Text("5")
+                Text("beamman")
+                Text("100")
+            }
         }
+        Text(".\n.\n.\nIf you want to leave your name, be stronger.")
+        Button()
     }
 }
 
