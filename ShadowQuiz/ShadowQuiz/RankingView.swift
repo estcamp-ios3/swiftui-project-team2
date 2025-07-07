4edfd1df3a06be9f0deead63afc7b2ad46179c2d
권태우 TaewooKéplerKwon <kwontaewoo0901@gmail.com>
2025-07-07 13:21:05 +0900

    Update RankingView.swift

--- ShadowQuiz/ShadowQuiz/RankingView.swift
+++ ShadowQuiz/ShadowQuiz/RankingView.swift
@@ -9,51 +9,57 @@ import SwiftUI
 
 struct RankingView: View {
     var body: some View {
-        Text("RANKING")
-            .font(.largeTitle)
-            .padding()
-        Grid{
-            GridRow{
-                Text("Rank")
-                Text("Nickname")
-                Text("Score")
-            }
-            .padding()
-            
-            GridRow{
-                Text("1")
-                Text("MIDKING")
-                Text("300")
-            }
-            GridRow{
-                Text("2")
-                Text("JUGKING")
-                Text("270")
-            }
-            GridRow{
-                Text("3")
-                Text("hide on bush")
-                Text("200")
-            }
-            GridRow{
-                Text("4")
-                Text("CJ CloudTempler")
-                Text("180")
-            }
-            GridRow{
-                Text("5")
-                Text("beamman")
-                Text("100")
+        ZStack {
+            Color("CBEFB9")
+                .ignoresSafeArea()
+            VStack{
+                Text("RANKING")
+                    .font(.largeTitle)
+                    .padding()
+                Grid{
+                    GridRow{
+                        Text("Rank")
+                        Text("Nickname")
+                        Text("Score")
+                    }
+                    .padding()
+                    
+                    GridRow{
+                        Text("1")
+                        Text("MIDKING")
+                        Text("300")
+                    }
+                    GridRow{
+                        Text("2")
+                        Text("JUGKING")
+                        Text("270")
+                    }
+                    GridRow{
+                        Text("3")
+                        Text("hide on bush")
+                        Text("200")
+                    }
+                    GridRow{
+                        Text("4")
+                        Text("CJ CloudTempler")
+                        Text("180")
+                    }
+                    GridRow{
+                        Text("5")
+                        Text("beamman")
+                        Text("100")
+                    }
+                }
+                Text("If you want to leave your name, be stronger.")
+                    .padding()
+                Button("Home"){
+                    
+                }
+                .foregroundStyle(.white)
+                .frame(width: 200, height: 50)
+                .background(Color.black.opacity(0.5))
             }
         }
-        Text("If you want to leave your name, be stronger.")
-            .padding()
-        Button("Home"){
-            
-        }
-        .foregroundStyle(.white)
-        .frame(width: 200, height: 50)
-        .background(Color.black.opacity(0.5))
     }
 }
 
