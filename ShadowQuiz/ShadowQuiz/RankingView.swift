485a027fb232142bc8e32795608f9d00adfaaced
권태우 TaewooKéplerKwon <kwontaewoo0901@gmail.com>
2025-07-07 10:44:32 +0900

    Create RankingView.swift

---
+++ ShadowQuiz/ShadowQuiz/RankingView.swift
@@ -0,0 +1,22 @@
+//
+//  RankingView.swift
+//  ShadowQuiz
+//
+//  Created by 권태우 on 7/7/25.
+//
+
+import SwiftUI
+
+struct RankingView: View {
+    var body: some View {
+        Text("RANKING")
+            .font(.largeTitle)
+        Grid{
+            
+        }
+    }
+}
+
+#Preview {
+    RankingView()
+}
