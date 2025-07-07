969d471c49a96122aac9c44754961f8c221e63de
JongHyuck-Lim <racek0801@gmail.com>
2025-07-07 15:50:18 +0900

    게임 결과 화면

---
+++ ShadowQuiz/ShadowQuiz/MainView.swift
@@ -0,0 +1,18 @@
+//
+//  MainView.swift
+//  ShadowQuiz
+//
+//  Created by jonghyuck on 7/7/25.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        Text("메인")
+    }
+}
+
+#Preview {
+    MainView()
+}
