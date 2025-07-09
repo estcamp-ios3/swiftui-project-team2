8fc35f7fa42d0900d53b3e3bd6f9c3177d2ba172
JongHyuck-Lim <racek0801@gmail.com>
2025-07-09 10:42:49 +0900

    Update ResultView.swift

--- ShadowQuiz/ShadowQuiz/ResultView.swift
+++ ShadowQuiz/ShadowQuiz/ResultView.swift
@@ -8,27 +8,14 @@
 import SwiftUI
 import SwiftData
 
-//@Model
-//class Ranking {
-//    @Attribute(.unique) var id: UUID = UUID()
-//    var nickName: String
-//    var score: Int
-//    
-//    // 초기화
-//    init(id: UUID = UUID(), nickName: String, score: Int) {
-//        self.id = id
-//        self.nickName = nickName
-//        self.score = score
-//    }
-//}
-
 struct ResultView: View {
     
-    
+    @Query var rankings: [Ranking]
     @State private var nickName: String = ""
     @State private var score: Int = 0
     @State private var navigateToRanking = false
-
+    @Environment(\.modelContext) private var modelContext
+    
     
     var message: String {
         if score >= 150 {
@@ -40,7 +27,6 @@ struct ResultView: View {
         }
     }
     
-    @Environment(\.modelContext) private var modelContext
     
     func insertRanking() {
         let newRanking = Ranking(nickName: nickName, score: score)
@@ -51,86 +37,88 @@ struct ResultView: View {
             print("Failed to save: \(error)")
         }
     }
-        
-        
-        let backgroundColor: Color = Color(red: 203/255, green: 239/255, blue: 185/255)
-        
-        var body: some View {
-            NavigationStack {
-                ZStack{
-                    backgroundColor.ignoresSafeArea()
-                    VStack {
-                        Text("GAME RESULT")
-                            .padding()
-                            .font(.system(size: 50, weight: .bold))
-                            .fontDesign(.rounded)
-                            .foregroundColor(Color.black.opacity(0.5))
-                        
-                        Text("Your Total Score is")
-                            .font(.system(size: 25, weight: .bold))
-                            .fontDesign(.rounded)
-                            .foregroundColor(Color.black.opacity(0.7))
-                        
-                        Text("\(score)")
-                            .font(.system(size: 100, weight: .bold))
-                            .fontDesign(.rounded)
-                            .foregroundColor(Color.black)
-                        
-                        if score >= 100 {
-                            Text(message)
+    
+    
+    let backgroundColor: Color = Color(red: 203/255, green: 239/255, blue: 185/255)
+    
+    var body: some View {
+        NavigationStack {
+            ZStack{
+                backgroundColor.ignoresSafeArea()
+                VStack {
+                    Text("GAME RESULT")
+                        .padding()
+                        .font(.system(size: 50, weight: .bold))
+                        .fontDesign(.rounded)
+                        .foregroundColor(Color.black.opacity(0.5))
+                    
+                    Text("Your Total Score is")
+                        .font(.system(size: 25, weight: .bold))
+                        .fontDesign(.rounded)
+                        .foregroundColor(Color.black.opacity(0.7))
+                    
+                    Text("\(score)")
+                        .font(.system(size: 100, weight: .bold))
+                        .fontDesign(.rounded)
+                        .foregroundColor(Color.black)
+                    
+                    if score >= 100 {
+                        Text(message)
+                            .font(.system(size: 45).bold())
+                    } else if score < 100 {
+                        NavigationLink(destination: MainView()) {
+                            Text("Try again🙁")
                                 .font(.system(size: 45).bold())
-                        } else if score < 100 {
-                            NavigationLink(destination: MainView()) {
-                                Text("Try again🙁")
-                                    .font(.system(size: 45).bold())
-                            }
-                        }
-                        Divider()
-                        Spacer()
-                        HStack {
-                            Spacer()
-                            NavigationLink(destination: MainView()) {
-                                RoundedRectangle(cornerRadius: 20)
-                                    .fill(Color.black.opacity(0.2))
-                                    .frame(width: 150, height: 100)
-                                    .overlay(Text("Go to HOME")
-                                        .font(.largeTitle))
-                            }
-                            Spacer()
-                            Rectangle()
-                                .frame(width: 1, height: 100)
-                            Spacer()
-                            NavigationLink(destination: RankingView()) {
-                                RoundedRectangle(cornerRadius: 20)
-                                    .fill(Color.black.opacity(0.2))
-                                    .frame(width: 150, height: 100)
-                                    .overlay(Text("Check the\nRANKING")
-                                        .font(.largeTitle))
-                            }
-                            Spacer()
                         }
                     }
-                    VStack {
-                        TextField("Write your Nickname", text: $nickName)
-                            .textFieldStyle(.roundedBorder)
-                            .padding(.horizontal)
-                        
-                        Button {
-                            insertRanking()
-                            navigateToRanking = true
-                        } label: {
-                            Text("Upload")
+                    Divider()
+                    Spacer()
+                    HStack {
+                        Spacer()
+                        NavigationLink(destination: MainView()) {
+                            RoundedRectangle(cornerRadius: 20)
+                                .fill(Color.black.opacity(0.2))
+                                .frame(width: 150, height: 100)
+                                .overlay(Text("Go to HOME")
+                                    .font(.largeTitle))
                         }
-
-                        NavigationLink(destination: RankingView(), isActive: $navigateToRanking) {
-                            EmptyView()
+                        Spacer()
+                        Rectangle()
+                            .frame(width: 1, height: 100)
+                        Spacer()
+                        NavigationLink(destination: RankingView()) {
+                            RoundedRectangle(cornerRadius: 20)
+                                .fill(Color.black.opacity(0.2))
+                                .frame(width: 150, height: 100)
+                                .overlay(Text("Check the\nRANKING")
+                                    .font(.largeTitle))
                         }
+                        Spacer()
+                    }
+                }
+                VStack {
+                    TextField("Write your Nickname", text: $nickName)
+                        .textFieldStyle(.roundedBorder)
+                        .padding(.horizontal)
+                    
+                    Button {
+                        insertRanking()
+                        navigateToRanking = true
+                    } label: {
+                        Text("Upload")
+                            .font(.title2)
+                            .padding()
+                            .background(Color.blue.opacity(0.3))
+                            .cornerRadius(10)
                     }
                 }
             }
+            .navigationDestination(isPresented: $navigateToRanking) {
+                RankingView()
+            }
         }
     }
-
+}
 
 
 #Preview {
