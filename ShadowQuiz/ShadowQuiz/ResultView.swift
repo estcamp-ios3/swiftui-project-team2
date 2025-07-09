53d77f504c11251b75056b191f222475de72a5c4
JongHyuck-Lim <racek0801@gmail.com>
2025-07-09 16:03:28 +0900

    Update ResultView.swift

--- ShadowQuiz/ShadowQuiz/ResultView.swift
+++ ShadowQuiz/ShadowQuiz/ResultView.swift
@@ -27,14 +27,14 @@ struct ResultView: View {
             return "Well Done😊"
         }
     }
-    
+
     
     func insertRanking() {
         let newRanking = Rank(nickName: nickName, score: score, subject: subject)
         modelContext.insert(newRanking)
         do {
             try modelContext.save()
-            print("Success to save: \(newRanking.nickName), \(newRanking.score)")
+            print("Success to save: \(newRanking.nickName), \(newRanking.score), \(newRanking.subject)")
         } catch {
             print("Failed to save: \(error)")
         }
@@ -60,7 +60,7 @@ struct ResultView: View {
                         .foregroundColor(Color.black.opacity(0.7))
                     
                     Text("\(score)")
-                        .font(.system(size: 100, weight: .bold))
+                        .font(.system(size: 150, weight: .bold))
                         .fontDesign(.rounded)
                         .foregroundColor(Color.black)
                     
@@ -68,16 +68,40 @@ struct ResultView: View {
                         Text(message)
                             .font(.system(size: 45).bold())
                     } else if score < 100 {
-                        NavigationLink(destination: MainView()) {
                             Text("Try again🙁")
                                 .font(.system(size: 45).bold())
                                 .foregroundStyle(.gray)
                         }
-                    }
                     Spacer()
                     Divider()
-                    HStack {
+                    Spacer()
+                    Spacer()
+                    VStack {
                         Spacer()
+                        Text("Upload your Score and Nickname🖋️")
+                            .frame(width: 350, alignment: .leading)
+                        TextField("Write your Nickname", text: $nickName)
+                            .frame(width: 370)
+                            .textFieldStyle(.roundedBorder)
+                            .padding(.horizontal)
+                        
+                        Button {
+                            insertRanking()
+                            navigateToRanking = true
+                        } label: {
+                            Text("Upload")
+                                .font(.title3)
+                                .frame(width: 70, height: 0)
+                                .padding()
+                                .background(Color.black.opacity(0.2))
+                                .foregroundStyle(.white)
+                                .cornerRadius(0)
+                        }
+                    }
+                    VStack {
+                        Rectangle()
+                            .fill(Color.clear.opacity(0.2))
+                            .frame(width: 0, height: 130)
                         NavigationLink(destination: MainView()) {
                             Rectangle()
                                 .fill(Color.black.opacity(0.2))
@@ -86,11 +110,9 @@ struct ResultView: View {
                                     .foregroundStyle(.white)
                                     .font(.system(size: 30)))
                         }
-                        Spacer()
                         Rectangle()
                             .fill(Color.gray.opacity(0.5))
-                            .frame(width: 0.5, height: 60)
-                        Spacer()
+                            .frame(width: 170, height: 0.5)
                         NavigationLink(destination: RankingView(selectedSubject: subject)) {
                             Rectangle()
                                 .fill(Color.black.opacity(0.2))
@@ -101,29 +123,6 @@ struct ResultView: View {
                         }
                         Spacer()
                     }
-                }
-                VStack {
-                    Spacer()
-                    Text("Upload your Score and Nickname🖋️")
-                        .frame(width: 350, alignment: .leading)
-                    TextField("Write your Nickname", text: $nickName)
-                        .frame(width: 370)
-                        .textFieldStyle(.roundedBorder)
-                        .padding(.horizontal)
-                    
-                    Button {
-                        insertRanking()
-                        navigateToRanking = true
-                    } label: {
-                        Text("Upload")
-                            .font(.title3)
-                            .frame(width: 70, height: 0)
-                            .padding()
-                            .background(Color.black.opacity(0.3))
-                            .foregroundStyle(.white)
-                            .cornerRadius(0)
-                    }
-                    Spacer()
                 }
             }
             .navigationDestination(isPresented: $navigateToRanking) {
